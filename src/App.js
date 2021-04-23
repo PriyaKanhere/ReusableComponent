a765827169e42ab74b54ab15e109953797e2c6a4
PriyaKanhere <kanhere.priya@gmail.com>
2021-04-23 17:49:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDom from "react-dom"
+import "./style.css"
+
+const Badge = ({ children, color, ...props }) => {
+  return (
+    <span className={`badge ${color}`} {...props}>{children}</span>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><section>
+      <h1>Check out these badges!  </h1>
+      <h3>these are calling same Badge which is a reusable component..Click on yellow and green only they will generate alert</h3>
+      <Badge color="green">Success</Badge> This is operational. <br />
+      <Badge color="red" >Removed</Badge> This is critical. <br />
+      <Badge color="yellow" onClick={() => alert("yellow  was clicked")}>Warning</Badge> This is a warning. <br />
+      <Badge color="blue"
+        onClick={() => alert("Blue was clicked")}
+
+      >Beta</Badge> This is in progress. <br />
+    </section></div>
   );
 }
 
